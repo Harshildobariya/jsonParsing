b09e21cd1bb689e467477041f8871272e30c7d96
Harshil Dobariya <harshildobariya111@hail.com>
2019-01-11 00:01:35 +0530

    first commit

---
+++ jsonParsing/DetailVC.swift
@@ -0,0 +1,30 @@
+//
+//  DetailVC.swift
+//  jsonParsing
+//
+//  Created by Harshil sureshbhai dobariya on 08/01/19.
+//  Copyright © 2019 Harshil sureshbhai dobariya. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ jsonParsing/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  jsonParsing
+//
+//  Created by Harshil sureshbhai dobariya on 08/01/19.
+//  Copyright © 2019 Harshil sureshbhai dobariya. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
